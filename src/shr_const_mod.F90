5ae9154911206aafbf73c6c8a0e500e5ec6907a9
Jim Edwards <jedwards@ucar.edu>
2021-11-09 10:41:12 -0600

    port to ihesp codebase

--- src/shr_const_mod.F90
+++ src/shr_const_mod.F90
@@ -1,16 +1,12 @@
-!===============================================================================
-! SVN $Id: shr_const_mod.F90 61510 2014-06-26 21:58:56Z tcraig $
-! SVN $URL: https://svn-ccsm-models.cgd.ucar.edu/csm_share/trunk_tags/share3_150116/shr/shr_const_mod.F90 $
-!===============================================================================
-
 MODULE shr_const_mod
 
-   use shr_kind_mod, only : R8 => shr_kind_r8
+   use shr_kind_mod
+
+   integer(SHR_KIND_IN),parameter,private :: R8 = SHR_KIND_R8 ! rename for local readability only
 
    !----------------------------------------------------------------------------
    ! physical constants (all data public)
    !----------------------------------------------------------------------------
-   private :: R8
    public
 
    real(R8),parameter :: SHR_CONST_PI      = 3.14159265358979323846_R8  ! pi
@@ -36,7 +32,6 @@ MODULE shr_const_mod
    real(R8),parameter :: SHR_CONST_TKTRIP  = 273.16_R8       ! triple point of fresh water        ~ K
    real(R8),parameter :: SHR_CONST_TKFRZ   = 273.15_R8       ! freezing T of fresh water          ~ K
    real(R8),parameter :: SHR_CONST_TKFRZSW = SHR_CONST_TKFRZ - 1.8_R8 ! freezing T of salt water  ~ K
-   real(R8),parameter :: SHR_CONST_ZSRFLYR = 3.0_R8          ! ocn surf layer depth for diurnal SST cal ~ m
 
    real(R8),parameter :: SHR_CONST_RHODAIR = &               ! density of dry air at STP  ~ kg/m^3
                          SHR_CONST_PSTD/(SHR_CONST_RDAIR*SHR_CONST_TKFRZ)
@@ -53,41 +48,18 @@ MODULE shr_const_mod
    real(R8),parameter :: SHR_CONST_LATVAP  = 2.501e6_R8      ! latent heat of evaporation ~ J/kg
    real(R8),parameter :: SHR_CONST_LATSUB  = &               ! latent heat of sublimation ~ J/kg
                          SHR_CONST_LATICE + SHR_CONST_LATVAP
-   real(R8),parameter :: SHR_CONST_CONDICE = 2.1_R8          ! thermal conductivity of ice ~ W/m/K
-   real(R8),parameter :: SHR_CONST_KAPPA_LAND_ICE = &        ! Diffusivity of heat in land ice ~
-                         SHR_CONST_CONDICE / (SHR_CONST_RHOICE*SHR_CONST_CPICE)
-   real(R8),parameter :: SHR_CONST_TF0    = 6.22e-2_R8       ! The freezing temperature at zero pressure in
-   ! sub-ice-shelf ocean cavities ~ C
-   real(R8),parameter :: SHR_CONST_DTF_DP = -7.43e-8_R8      ! The coefficient for the term proportional to the (limited)
-   ! pressure in the freezing temperature in sub-ice-shelf ocean cavities. ~ C Pa^{-1}
-   real(R8),parameter :: SHR_CONST_DTF_DS = -5.63e-2_R8      !The coefficient for the term proportional to salinity in
-   ! the freezing temperature in sub-ice-ice ocean cavities ~ C PSU^{-1}
-   real(R8),parameter :: SHR_CONST_DTF_DPDS = -1.74e-10_R8   ! The coefficient for the term proportional to salinity times
-   ! pressure in the freezing temperature in sub-ice-shelf ocean cavities ~ C PSU^{-1} Pa^{-1}
    real(R8),parameter :: SHR_CONST_OCN_REF_SAL = 34.7_R8     ! ocn ref salinity (psu)
    real(R8),parameter :: SHR_CONST_ICE_REF_SAL =  4.0_R8     ! ice ref salinity (psu)
 
-   real(R8),parameter :: SHR_CONST_SPVAL        = 1.0e30_R8                 ! special missing value
-   real(R8),parameter :: SHR_CONST_SPVAL_TOLMIN = 0.99_R8 * SHR_CONST_SPVAL ! min spval tolerance
-   real(R8),parameter :: SHR_CONST_SPVAL_TOLMAX = 1.01_R8 * SHR_CONST_SPVAL ! max spval tolerance
-   real(R8),parameter :: SHR_CONST_SPVAL_AERODEP= 1.e29_r8                  ! special aerosol deposition
-
-   !Water Isotope Ratios in Vienna Standard Mean Ocean Water (VSMOW):
-   real(R8),parameter :: SHR_CONST_VSMOW_18O   = 2005.2e-6_R8   ! 18O/16O in VMSOW
-   real(R8),parameter :: SHR_CONST_VSMOW_17O   = 379.e-6_R8   ! 18O/16O in VMSOW
-   real(R8),parameter :: SHR_CONST_VSMOW_16O   = 0.997628_R8    ! 16O/Tot in VMSOW
-   real(R8),parameter :: SHR_CONST_VSMOW_D   = 155.76e-6_R8   ! 2H/1H in VMSOW
-   real(R8),parameter :: SHR_CONST_VSMOW_T   = 1.85e-6_R8  ! 3H/1H in VMSOW
-   real(R8),parameter :: SHR_CONST_VSMOW_H   = 0.99984426_R8  ! 1H/Tot in VMSOW
-   ! For best numerics in CAM5
-   real(R8),parameter :: SHR_CONST_RSTD_H2ODEV   = 1.0_R8      ! Rstd Dev Use
+   real(R8),parameter :: SHR_CONST_SPVAL      = 1.0e30_R8     ! special missing value
+   real(R8),parameter :: SHR_CONST_SPVAL_TOLMIN = 0.99_R8 * SHR_CONST_SPVAL  ! min spval tolerance
+   real(R8),parameter :: SHR_CONST_SPVAL_TOLMAX = 1.01_R8 * SHR_CONST_SPVAL  ! max spval tolerance
 
 contains
 
 !-----------------------------------------------------------------------------
 
-  elemental logical function shr_const_isspval(rval)
-!$omp declare simd(shr_const_isspval)
+  logical function shr_const_isspval(rval)
 
      real(r8), intent(in) :: rval
 
